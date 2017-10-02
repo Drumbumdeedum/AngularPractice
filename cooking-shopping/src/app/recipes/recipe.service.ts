b6e3e0dcf576275e2890090a54150e6ded13ea85
Daniel Szalay <sza.dan.uj@gmail.com>
2017-10-02 18:29:40 +0200

    use service in recipes components refactor

--- cooking-shopping/src/app/recipes/recipe.service.ts
+++ cooking-shopping/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from './recipe.model';
+import {EventEmitter} from "@angular/core";
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('First test recipe', 'This is a test', 'http://img.www.nosalty.hu/files/imagecache/recept/recept_kepek/cover_post_20.jpg'),
     new Recipe('Second test recipe', 'This is also a test', 'http://nagyireceptje.com/kepek/hortobagyi-husos-palacsinta-575.jpg')
